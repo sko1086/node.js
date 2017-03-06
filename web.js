19fdae086081b47aa20ddadf0f19f863fb67c3ff
node.js <sko1086@naver.com>
2017-03-06 23:42:51 +0900

    aa

--- web.js
+++ web.js
@@ -7,8 +7,12 @@ app.set('view engine', 'jade');
 app.get('/test', function(req, res){
   res.send('test');
 });*/
-http.createServer(function (req, res) {
-    /*res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');*/
-}).listen(8002);
-console.log('Server running at Cafe24');
+var server = http.createServer(function(req, res){
+  res.writeHead('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+
+});
+server.listen(port, hostname function(){
+      console.log(`Server running at http://${hostname}:${port}/`);
+
+});
