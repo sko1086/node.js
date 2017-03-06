0937b15698acd2ad6dd2ea1adf00cadcd67f4900
node.js <sko1086@naver.com>
2017-03-06 15:50:02 +0900

    test2

--- web.js
+++ web.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(8002);
-console.log('Server running at Cafe24');
+console.log('Server running at Cafe24!!!!');
