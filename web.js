f75a7970d6146b9fb9aa54f8c77815f57c376ea8
node.js <sko1086@naver.com>
2017-03-06 23:36:36 +0900

    dd

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+/*app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.get('/test', function(req, res){
   res.send('test');
-});
+});*/
 http.createServer(function (req, res) {
     /*res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');*/
