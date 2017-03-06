6ae3aef1d5ac6bd8188aa22897f3fe909ad5f04b
node.js <sko1086@naver.com>
2017-03-06 16:49:10 +0900

    test2

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8002);
+console.log('Server running at Cafe24');
 app.get('/test', function(req, res){
     res.send("test");
 };
-app.listen(8002, function(){
-  console.log('conected 3000!');
-});
