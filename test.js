b4fee0a4ca13cc2f7b5bd3042dc8fdd3fd0e9da0
node.js <sko1086@naver.com>
2017-03-06 18:09:31 +0900

    web

--- test.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/test', function(req, res){
-  res.send('test');
-});
--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-
+app.get('/test', function(req, res){
+  res.send('test');
+});
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
