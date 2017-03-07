1ff440f56b69557688938a8d0367a3d87ec0317c
node.js <sko1086@naver.com>
2017-03-07 12:40:08 +0900

    aaa

--- web.js
+++ web.js
@@ -1,18 +1,13 @@
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-/*app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.get('/test', function(req, res){
   res.send('test');
-});*/
-var server = http.createServer(function(req, res){
-  res.writeHead('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-
 });
-server.listen(8002, 'cout' function(){
-      console.log('hello');
-
+app.listen(8002, function () {
+  console.log('Example app listening on port 3000!');
 });
